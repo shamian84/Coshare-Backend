eaf1d4fa0dc3c6a2e37fb41c274393cdd5c555e4
Shami Alam <161938299+shamian84@users.noreply.github.com>
2025-12-17 16:25:21 +0530

    Check and create upload directory if missing
    Ensure upload directory exists before starting server.

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
 import multer from "multer";
-import path from "path"; 
+import path from "path";
+import fs from "fs";
 
 dotenv.config();
 
@@ -39,7 +40,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message || "Internal Server Error" });
 });
 
-
+const uploadDir = "./uploads";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
